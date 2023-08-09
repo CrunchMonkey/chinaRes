b66d2248f351107669ff0a3e6fd672346dbf16ef
이승렬 <dltmdfuf95@naver.com>
2023-08-09 20:13:20 +0900

    test

---
+++ src/test/java/com/example/demo/ChinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
